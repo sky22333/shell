98c7221c15349b4ed3d92bdd4854d138739bb375
NewName <newemail@example.com>
2024-03-19 22:54:42 +0800

    Update hysteria.sh

--- hy2/hysteria.sh
+++ hy2/hysteria.sh
@@ -560,7 +560,7 @@ menu() {
     echo -e " ${GREEN}1.${PLAIN} 安装 Hysteria 2"
     echo -e " ${GREEN}2.${PLAIN} ${RED}卸载 Hysteria 2${PLAIN}"
     echo " -------------"
-    echo -e " ${GREEN}3.${PLAIN} 关闭、开启、重启 Hysteria 2"
+    echo -e " ${GREEN}3.${PLAIN} 关闭、重启 Hysteria 2"
     echo -e " ${GREEN}4.${PLAIN} 修改 Hysteria 2 配置"
     echo -e " ${GREEN}5.${PLAIN} 显示 Hysteria 2 配置文件"
     echo " -------------"
