a08b4d54c4f5584dfb3e661a0a4a6ee797bb27de
NewName <newemail@example.com>
2024-04-09 02:13:40 +0800

    Update Mbit.sh

--- Mbit.sh
+++ Mbit.sh
@@ -30,7 +30,7 @@ do
     tc class add dev "$INTERFACE" parent 1: classid 1:1 htb rate "${LIMIT}"mbit
     tc class add dev "$INTERFACE" parent 1:1 classid 1:12 htb rate "${LIMIT}"mbit
     tc filter add dev "$INTERFACE" protocol ip parent 1:0 prio 1 u32 match ip dport "$PORT" 0xffff flowid 1:12
-    echo -e "${GREEN}端口 $PORT 的带宽限制已设置为 ${LIMIT}Mbit。${NC}"
+    echo -e "${GREEN}网络接口 eth0 的端口 $PORT 带宽限制已设置为 ${LIMIT}Mbit。${NC}"
 done
 
 echo -e "${YELLOW}如果要更改配置请先清除限速规则：sudo tc qdisc del dev $INTERFACE root${NC}"
