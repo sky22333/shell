274a7f7e9a796971ada0867c735e4fde0584f5a1
NewName <newemail@example.com>
2024-04-11 01:12:37 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -76,7 +76,7 @@ EOF
 show_inbound_config() {
     local ip=$(curl -s http://ipinfo.io/ip)
     echo -e "${green}Vmess 节点链接:${none}"
-    echo "vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"TK节点定制\",\"add\":\"$ip\",\"port\":$PORT,\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"path\":\"/$RANDOM_PATH\",\"type\":\"none\",\"host\":\"\",\"tls\":\"\"}" | base64 -w 0)"
+    echo "vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"vmess+ws\",\"add\":\"$ip\",\"port\":$PORT,\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"path\":\"/$RANDOM_PATH\",\"type\":\"none\",\"host\":\"\",\"tls\":\"\"}" | base64 -w 0)"
 }
 
 create_config
