b579cf79a2413b5c38e58c4dccce1aeba267f33c
NewName <newemail@example.com>
2024-08-05 19:30:10 +0800

    Update kaiji.sh

--- kaiji.sh
+++ kaiji.sh
@@ -2,9 +2,7 @@
 
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-sudo bash -c 'echo "127.0.0.1 wovow" >> /etc/hosts'
-
-sudo hostnamectl set-hostname wovow
+sudo hostnamectl set-hostname localhost
 
 # 清空 /etc/motd 文件并设置内容
 cat << 'EOF' > /etc/motd
