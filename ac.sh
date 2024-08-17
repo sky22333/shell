a1e5edef099ef8706347966d33ca1cfb1fa4b31d
NewName <newemail@example.com>
2024-08-18 06:06:45 +0800

    Update ac.sh

--- ac.sh
+++ ac.sh
@@ -41,7 +41,7 @@ fi
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
-
+sudo apt update -q
 # 检查操作系统
 if grep -Eqi "debian" /etc/issue || grep -Eqi "debian" /proc/version; then
     OS="debian"
