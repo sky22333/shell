9775c2cf0e571956c8b04cc79f0076f3dd6aba82
NewName <newemail@example.com>
2024-08-17 08:32:14 +0800

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 # 一键部署WordPress脚本
 
-# 检测操作系统类型
-OS=$(lsb_release -si)
-
-# 检查操作系统是否为 Debian 或 Ubuntu
-if [[ "$OS" != "Debian" && "$OS" != "Ubuntu" ]]; then
-    echo -e "\033[31m该脚本仅支持在 Debian 或 Ubuntu 系统上运行。\033[0m"
-    exit 1
-fi
-
 # 检查是否已经安装 WordPress
 if [ -f "/var/www/html/wordpress/wp-config.php" ]; then
     echo -e "\033[32mWordPress 已经安装。\033[0m"
