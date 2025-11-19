7f0e712c153ec9d408471d23d25188d299eb620b
starry <115192496+sky22333@users.noreply.github.com>
2025-11-19 09:17:00 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -2,21 +2,16 @@
 
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
-PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-homeproxy"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES luci-app-ttyd"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-app-diskman"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-PACKAGES="$PACKAGES luci-app-filemanager"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 
 # docker(需要1G以上空间)
-# PACKAGES="$PACKAGES luci-app-dockerman"
 # PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 
 make image PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE="512"
