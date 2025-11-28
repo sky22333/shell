d509dc59c4c72c2ce75d3c04c9ddf50bc8490b4d
starry <115192496+sky22333@users.noreply.github.com>
2025-11-28 21:54:57 +0800

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,5 @@
+# 测试：https://github.com/KANIKIG/Multi-EasyGost
+  wget -q --no-check-certificate "${gh_proxy_prefix}https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service"
+wget -q --no-check-certificate "${gh_proxy_prefix}https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service"
+wget -q --no-check-certificate "https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service"
+wget -q --no-check-certificate https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service
