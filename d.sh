8eb43b9343b9e1499e326437eaa45a1f2c85388e
starry <115192496+sky22333@users.noreply.github.com>
2025-11-28 21:57:03 +0800

    Update and rename demo.sh to d.sh

--- d.sh
+++ d.sh
@@ -3,3 +3,5 @@
 wget -q --no-check-certificate "${gh_proxy_prefix}https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service"
 wget -q --no-check-certificate "https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service"
 wget -q --no-check-certificate https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service
+
+wget -q --no-check-certificate "${gh_proxy_prefix} https://raw.githubusercontent.com/sky22333/shell/main/gost/gost.service"
