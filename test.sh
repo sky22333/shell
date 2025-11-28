c7c6aeff5e35835d1475c3e8336142c6cbb96749
starry <115192496+sky22333@users.noreply.github.com>
2025-11-28 21:30:31 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
 https://gh-proxy.com/https://github.com/sky22333/shell/new/main
+
+https://super-duper-palm-tree-g444p6wg7vpfwwjr-5000.app.github.dev/https://raw.githubusercontent.com/sky22333/shell/refs/heads/main/test.sh
+
+https://github.com/sky22333/shell/new/main
+
+https://wget.com/https://github.com/sky22333/shell/new/main
+
+gh-proxy.com/https://github.com/sky22333/shell/new/main
+
+wget.com/https://github.com/sky22333/shell/new/main
